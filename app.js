64533f7ef570f62413187763c29cbc7313b20725
csolar <cristiano@neogence.(none)>
2012-04-27 15:42:03 +0100

    added app parameters

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 /*
  * Instantiate munger
  */
-var munger     = require("./munger"),
-    translator = require("./pirate");
+var munger     = require("./munger");//,
+//    translator = require("./pirate");
     
     munger.init({
-        appKey : "1234x",
-        translator: pirate
+        consumer_key : "OkDRwz5gBGGIdMPRUS2kw",
+        consumer_secret: "15toQ2udzJXdFIysbKGK959Z4Vx40lL5z7WPD1wblQ",
+		access_token_key: "258811766-1rNhVMu3zvXB59d2cSceyq034EnSxHeunkdL3bo7",
+		access_token_secret: "gYWGtywbWuDvMDmNTQaRNGshNcO3aryjnAqQSdmSg",
+		originalTwitterAccount: "danomurray",
+		mungedTwitterAccount: "yyy",
+		userAgentAccount: "xxx@yyy.com",
+		newTweetCount : 10,
+		ignoreRetweets: true,
+		translations : "pirate",
+		twitterConsumerKey: "x",
+		twitterConsumerSecret : "x",
+		twitterConsumerOauthToken : "x",
+		twitterConsumerOauthSecret : "x"
+	//	translator: pirate
     });

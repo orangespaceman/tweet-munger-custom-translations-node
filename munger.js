b903e7fcbbd44a7850996d4f0794a1816d25b63b
Pete Goodman <thegingerbloke@gmail.com>
2012-04-27 23:00:16 +0100

    tweaking munger messages

--- munger.js
+++ munger.js
@@ -18,38 +18,42 @@ function init(data) {
 
   // init translator
   translator = require('./translations/' + data['translations']);
+  console.log("initialising tweet munger " + data['translations'] + " translations");
+
 
   // init twit
   var twit = new twitter({
-    consumer_key: data['consumer_key'],
-    consumer_secret: data['consumer_secret'],
-    access_token_key: data['access_token_key'],
-    access_token_secret: data['access_token_secret']
+    consumer_key        : data['consumer_key'],
+    consumer_secret     : data['consumer_secret'],
+    access_token_key    : data['access_token_key'],
+    access_token_secret : data['access_token_secret']
   });
 
 
   // get the user's ID from their name
 	twit.get('/users/show/' + data['originalTwitterAccount'] + '.json', function(data) {
-		console.log('user id is: ' + data.id);
+		console.log('user id for '+data.name+' ('+data.screen_name+') is: ' + data.id);
 
     // connect to the stream
 		twit.stream('statuses/filter', {follow:data.id}, function(stream) {
+
+      console.log("connected to twitter stream, waiting for tweets");
       
       // when a new tweet is detected
       stream.on('data', function(data) {
-        console.log(util.inspect(data));
+        console.log("new tweet detected: "+data.text);
       
-      // mung and repost
-      var translated = translator.translate(data.text);
-				twit.updateStatus(translated,
-					function(data) {
-						console.log(util.inspect(data));
-					}
-				);
-			});
-		});
-	});
+        // mung and repost
+        var translated = translator.translate(data.text);
+        twit.updateStatus(translated,
+          function(data) {
+            console.log("munged tweet posted: "+translated);
+            //console.log(util.inspect(data));
+          }
+        );
+      });
+    });
+  });
 }
 
-// export init method
 module.exports.init = init;

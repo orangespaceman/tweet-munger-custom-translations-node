fea5cfc6ffc7c5432bf1651e3405339e96952e05
csolar <cristiano@neogence.(none)>
2012-04-27 15:42:29 +0100

    added twitter user stream code

--- munger.js
+++ munger.js
@@ -1,11 +1,28 @@
 /*
  * The brains of the operation
  */
-function init() {
 
+var util = require('util'),
+	twitter = require('twitter');
+	
+function init(data) {
+	console.log(util.inspect(data));
+	 var twit = new twitter({
+		consumer_key: data['consumer_key'],
+		consumer_secret: data['consumer_secret'],
+		access_token_key: data['access_token_key'],
+		access_token_secret: data['access_token_secret']
+	});
+	
+	// connect to twitter user stream
+	twit.stream('user', {track: data['originalTwitterAccount']}, function(stream) {
+	    stream.on('data', function(data) {
+	        console.log(util.inspect(data));
+	    });
+	    // Disconnect stream after five seconds
+	    //setTimeout(stream.destroy, 10000);
+	});
 }
 
 
-module.exports = {
-  init: init
-};
+module.exports.init = init;
